5c86bf5cf3adb82646d5c2c29a600d36355d6439
Tijo <hamsen3880@outlook.com>
2025-06-21 15:09:15 +0530

    text generate

--- frontend/components/ui/text-hover-effect.jsx
+++ frontend/components/ui/text-hover-effect.jsx
@@ -89,7 +89,7 @@ export const TextHoverEffect = ({
         textAnchor="middle"
         dominantBaseline="middle"
         strokeWidth="0.3"
-        className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-800"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-400"
         initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
         animate={{
           strokeDashoffset: 0,
